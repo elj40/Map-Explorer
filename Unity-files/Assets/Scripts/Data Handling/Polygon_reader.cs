a55b8717716ceca8335d1669273387b7c255576f
elj40 <66779575+elj40@users.noreply.github.com>
2022-02-20 18:40:41 +0200

    Split into many different polygons

--- Unity-files/Assets/Scripts/Data Handling/Polygon_reader.cs
+++ Unity-files/Assets/Scripts/Data Handling/Polygon_reader.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using Newtonsoft.Json;
+using TriangleNet;
 
 
 public class Polygon_reader : MonoBehaviour
 {   
     [SerializeField] string fileName = @"Places_polygon.geojson";
+
+    public GameObject polygonPrefab;
 
     public PolygonData polygonData;
     // Start is called before the first frame update
@@ -28,6 +31,26 @@ public class Polygon_reader : MonoBehaviour
 
         polygonData.countCoordinates();
         Debug.Log(polygonData.totalCoordinates);
+
+        createPolygons();
+    }
+
+    void createPolygons() {
+        int id = 0;
+
+        float offsetX = polygonData.features[0].geometry.coordinates[0][0][0][0];
+        float offsetZ = polygonData.features[0].geometry.coordinates[0][0][0][1];
+        
+        foreach (Feature f in polygonData.features) {
+            GameObject p = Instantiate(polygonPrefab, this.transform);
+            Polygon_drawer drawer = p.GetComponent<Polygon_drawer>();
+            drawer.id = id;
+            drawer.offsetX = offsetX;
+            drawer.offsetZ = offsetZ;
+            drawer.polygonReader = this;
+
+            id++;
+        }
     }
 
 }
