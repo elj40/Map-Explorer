70b31bc6fdb41d320b7d20185542e047bb99d624
elj40 <66779575+elj40@users.noreply.github.com>
2022-02-25 16:07:29 +0200

    Setup gets its own printing functions for arrays

--- Unity-files/Assets/Scripts/Setup.cs
+++ Unity-files/Assets/Scripts/Setup.cs
@@ -7,4 +7,40 @@ public class Setup : MonoBehaviour
     {
         Application.targetFrameRate = 60;
     }
+
+    public static void printArray(Vector3[] array) {
+    string s = "[";
+    foreach(Vector3 v in array) {
+        s += "(x: " + v.x.ToString() + ", y: " + v.y.ToString() + ", z:" + v.z.ToString() + "),";
+    }
+    s += "]";
+    Debug.Log(s);
+    }
+
+    public static void printArray(Vector2[] array) {
+    string s = "[";
+    foreach(Vector2 v in array) {
+        s += "(x: " + v.x.ToString() + ", y: " + v.y.ToString() + "),";
+    }
+    s += "]";
+    Debug.Log(s);
+    }
+
+    public static void printArray(int[] array) {
+        string s = "[";
+        foreach(float v in array) {
+            s += v.ToString() + ", ";
+        }
+        s += "]";
+        Debug.Log(s);
+    }
+
+    public static void print(Vector3 v, string description) {
+        Debug.Log(description + "(x: " + v.x.ToString() + ", y: " + v.y.ToString() + ", z:" + v.z.ToString() + "),");
+    }
+
+    public static void print(Vector2 v, string description) {
+        Debug.Log(description + "(x: " + v.x.ToString() + ", y: " + v.y.ToString() + "),");
+    }
 }
+
